40830ab9d6c75fdc5d806b4f8fa64f39a53e22ca
Akhil Reddy <reddya201@gmail.com>
2020-05-18 15:04:02 +0530

    Data Binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,4 +1,4 @@
-
+import {Comment} from './comment';
 export class Dish{
     id: string;
     name: string;
@@ -8,5 +8,6 @@ export class Dish{
     label: string;
     price: string;
     description: string;
+    comments: Comment[];
 
 }
